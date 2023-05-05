de260b4d5f60c5a3ba73b89b4fcae7a6d3573b5d
Hongjun Chi <45268286+l1l222@users.noreply.github.com>
2023-05-05 11:02:03 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -6,13 +6,30 @@ class StrengthFunction:
 
 	def compute_gradient(self):
 		return
-
 class CostFunction:
-	def compute_cost(self):
-		return
+    def __init__(self):
+        self.b = .00001
+        self.type = 1
+    
+    def calcCost(self,x):
+        if self.type==1:
+            return 1.0 /(1.0+math.exp(-x/self.b))
+    
+    def calcGradient(self,x):
+        if self.type==1:
+            tmp = 1.0 / (1+np.exp(x/self.b))
+            gradient = tmp * (1-tmp) / self.b
+            return gradient
+    def calcCostAndGradient(self,x):
+        if self.type==1:
+            cost = 1.0/(1.0+np.exp(-x/self.b))
+            gradient = cost * (1-cost) / self.b
+            return cost, gradient
 
-	def compute_gradient(self):
-		return
-
-	def compute_cost_and_gradient(self):
-		return
+class ParameterLearner:
+    def __init__(self):
+        self.strengthf=StrengthFunction()
+        self.costf=CostFunction()
+        self.alpha=0.3
+        self.time_limit=30
+        self.print_progress=True
