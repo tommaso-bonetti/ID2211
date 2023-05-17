1dd98765f85f717ef32c79e81c347bb4df191219
August Paulsrud <augustpaulsrud@gmail.com>
2023-05-17 16:38:35 +0200

    fixed pref and rand

--- pref.py
+++ pref.py
@@ -9,22 +9,45 @@ import snap
 
 #takes current graph and list of nodes to add
 #returns a graph with these nodes attatched by preferential attatchment
-#TODO, handel non connected nodes
-#TODO, should nodes be able to attatch to other nodes in the "nodes" list?
-#TODO, give self connected nodes a weight?
-#TODO, add 1 base weight
 def pref_add_nodes(adjacency, toAdd):
-    #new_graph = graph
-    #adjacency = graph.get_adj_matrix()
-    nodelen = len(adjacency)
-    extended = np.vstack([adjacency, [0]*len(adjacency)])
-    extended = np.column_stack([extended, [0]*len(extended)])
+    
+    nodeslen = len(adjacency)
+
+    #special case for 0
+    if(len(adjacency) == 0):
+        adjacency = [[1]]
+        toAdd = toAdd - 1
+
+    extended = [[0]*(nodeslen+toAdd)]*(nodeslen+toAdd)
+    extended = np.array(extended)
+    
+    weights = np.array(range(nodeslen))
+    incomming = np.sum(adjacency, axis=0)
+    for i in range(nodeslen):
+        while incomming[i] > 0:
+            np.append(weights,i)
+            incomming[i] = incomming[i] - 1
+    
+    for i in range(nodeslen):
+        for j in range(nodeslen):
+            extended[i][j] = adjacency[i][j]
 
     for i in (range(toAdd)):
         #select node
-        #TODO, check
-        weights = sum(adjacency, axsis = 1)/np.sum(adjacency)
-        connection = np.random.choice(nodelen+i, 1, weights[:nodelen+i])
-        adjacency[connection][i+nodelen] = 1
-        #nodelen += 1
+        weights = np.append(weights,i)
+        connection = np.random.choice(weights, 1)[0]
+        weights = np.append(weights,connection)
+        extended[i+nodeslen][connection] = 1
+
     return extended
+
+#testfunction
+def main():
+    adjacency = pref_add_nodes([[0,1],[0,1]], 1000)
+    #print(adjacency)
+    print(np.sum(adjacency))
+    print(max(np.sum(adjacency, axis=0)))
+    print(max(np.sum(adjacency, axis=1)))
+    
+if __name__ == '__main__':
+    main()
--- rand.py
+++ rand.py
@@ -9,21 +9,37 @@ import snap
 
 #takes current graph and list of nodes to add
 #returns a graph with these nodes attatched randomly
-#TODO, handel non connected nodes (add node as option to choose from and interpret self connection as non connection)
-#TODO, should nodes be able to attatch to other nodes in the "nodes" list? (recalculate length)
+
 def random_add_nodes(adjacency, toAdd):
-    #new_graph = graph
 
     nodeslen = len(adjacency)
-    extended = np.vstack([adjacency, [0]*len(adjacency)])
-    extended = np.column_stack([extended, [0]*len(extended)])
+
+    #special case for 0
+    if(len(adjacency) == 0):
+        adjacency = [[1]]
+        toAdd = toAdd - 1
+
+    extended = [[0]*(nodeslen+toAdd)]*(nodeslen+toAdd)
+    extended = np.array(extended)
+    
+    for i in range(nodeslen):
+        for j in range(nodeslen):
+            extended[i][j] = adjacency[i][j]              
     
     for i in (range(toAdd)):
         #pic a random number up to len of graph node lsit
         #if it is original graph or the new_graph decides if new nodes can be connected
-        connection = np.random.choice(nodeslen+i) 
-        extended[connection][i+nodeslen] = 1
-
+        connection = np.random.choice((nodeslen+i),1)[0] 
+        extended[i+nodeslen][connection] = 1
     return extended
 
+#testfunction
+def main():
+    adjacency = random_add_nodes([[]], 1000)
+    print(np.sum(adjacency))
+    print(max(np.sum(adjacency, axis=0)))
+    print(max(np.sum(adjacency, axis=1)))
 
+
+if __name__ == '__main__':
+    main()
