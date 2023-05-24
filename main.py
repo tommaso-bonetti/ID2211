783723930696b47f383e1fa8d068fedd48c6a85c
August Paulsrud <augustpaulsrud@gmail.com>
2023-05-24 16:27:02 +0200

    added code to save values to files

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import numpy as np
+from datetime import datetime
+import json
 
 from functions import CostFunction, StrengthFunction
 from graph import GraphData
@@ -28,6 +30,21 @@ def main():
 	# TODO: print to file the indices of training and test set and the learned parameters learned_w.
 	training_indices = instances.train_idx  # Dictionary in the form { rumor_number: list_of_indices }
 	test_indices = instances.test_idx  # Dictionary in the form { rumor_number: list_of_indices }
+
+	now = datetime.now()
+	current_time = now.strftime("%H_%M_%S")
+	
+	f_w = open("datafile_"+current_time+"_learned_w.txt", "a")
+	f_w.write(learned_w)
+	f_w.close()
+
+	f_training = open("datafile_"+current_time+"_training_indices.txt", "a")
+	f_training.write(json.dumps(training_indices))
+	f_training.close()
+
+	f_test = open("datafile_"+current_time+"_test_indices.txt", "a")
+	f_test.write(json.dumps(f_test))
+	f_test.close()
 
 	true, pred = instances.predict(strength, learned_w, alpha)
 	true, pred = np.array(true), np.array(pred)
